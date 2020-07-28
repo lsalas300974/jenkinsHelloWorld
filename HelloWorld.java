55cc6a37edfa6b0c6ee36026a0965e3dbb79e540
lsalas300974 <lsalas300974@gmail.com>
2020-07-28 10:41:36 -0600

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld{
  public static void main(String args[]){
-  System.out.println("hello to jenkins training Change 4 4 4 4 for GITHub");
+  System.out.println("Change github-webhook");
  }
 }
 
